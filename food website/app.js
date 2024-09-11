c4a0b5f9aaf5ca777a9438817fecf0f1b6fedeb3
Kashaf Naz <kashafnazk389@gmail.com>
2024-09-11 12:57:04 +0500

    Add files via upload

---
+++ food website/app.js
@@ -0,0 +1,8 @@
+const specialOfferBtn = document.getElementById('special-offer-btn');
+
+if (specialOfferBtn) {
+    specialOfferBtn.addEventListener('click', () => {
+        alert('Special Offer: Get 20% off on all orders this weekend!');
+    });
+}
+// Food items and prices
